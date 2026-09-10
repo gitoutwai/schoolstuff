d4cd4c107eb0235b24ea01135b29192afc22c6e9
gitoutwai <wai@kellettschool.edu.hk>
2026-09-10 13:20:40 +0800

    Finished palindrome checker

--- Y12/Miss Joseph/programming basics/pal.py
+++ Y12/Miss Joseph/programming basics/pal.py
@@ -1,4 +1,14 @@
-text = input("text: ")
+text = input("text: ").lower()
 
-rText = text[::-1]
-print(rText)
+final = ""
+
+for char in text:
+    if ord(char) > 96 and ord(char) < 124:
+        final += char
+
+rText = final[::-1]
+
+if rText == final:
+    print("Pally!")
+else:
+    print("No")
