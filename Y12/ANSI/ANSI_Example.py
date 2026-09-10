233f085a4d80df45cfabf3430015616ccb1e6bd0
gitoutwai <wai@kellettschool.edu.hk>
2026-09-10 13:08:41 +0800

    Mr Davies prototype

--- Y12/ANSI/ANSI_Example.py
+++ Y12/ANSI/ANSI_Example.py
@@ -3,18 +3,30 @@
 # https://www.compart.com/en/unicode/search?q=box+drawing#characters
 
 import sys
+import os
+
+def clear():
+    """
+    Clear the screen
+    """
+
+    print("\x1b[3J", end="")
+
+def move_to(coords):
+    # Move to coords
+    if coords[1] > 0:
+        y = str(coords[1])
+        sys.stdout.write(f"\033[{y}B") # Moves down
+
+    # x = str(coords[1])
+    # sys.stdout.write(f"\033[{x}C") # Moves right
 
 # Draw a box
-def draw_box(width, height, makeSquare, coords):
+def draw_box(width, height, coords, makeSquare=False, modifier="H"):
 
-    # Saves current location
-    sys.stdout.write("\033[s")
+    move_to(coords)
 
-    # Move to coords
-    stringCoords1 = str(coords[1])
-    sys.stdout.write(f"\033[{stringCoords1}B")
-
-    box_parts = {"TL":"╔", "TR":"╗", "BL":"╚", "BR":"╝", "V":"║", "H":"═"}
+    box_parts = {"HTL":"╔", "HTR":"╗", "HBL":"╚", "HBR":"╝", "HV":"║", "HH":"═"}
 
     if makeSquare:
         width *= 2
@@ -26,38 +38,26 @@ def draw_box(width, height, makeSquare, coords):
                 print("■")
             # TL
             elif x == 0+coords[0] and y == 0:
-                print(box_parts["TL"], end="")
+                print(box_parts[f"{modifier}TL"], end="")
             # TR
             elif x == width - 1 and y == 0:
-                print(box_parts["TR"], end="")
+                print(box_parts[f"{modifier}TR"], end="")
             # BL
             elif x == 0+coords[0] and y == height - 1:
-                print(box_parts["BL"], end="")
+                print(box_parts[f"{modifier}BL"], end="")
             # BR
             elif x == width - 1 and y == height - 1:
-                print(box_parts["BR"], end="")
+                print(box_parts[f"{modifier}BR"], end="")
             # Ver
             elif x == 0+coords[0] or x == width - 1:
-                print(box_parts["V"], end="")
+                print(box_parts[f"{modifier}V"], end="")
             # Hor
             elif (y == 0 or y == height - 1) and x >= 0+coords[0]:
-                print(box_parts["H"], end="")
+                print(box_parts[f"{modifier}H"], end="")
             # Empty
             else:
                 sys.stdout.write("\033[C")
-        print()
-
-    # Return to saved spot
-    sys.stdout.write("\033[u")
-
-# use function
-coords = [0,0]
-draw_box(4,4, makeSquare=True, coords=coords)
-
-coords = [2,2]
-draw_box(2,2, makeSquare=True, coords=coords)
-
-sys.stdout.write("\033[100A")
+        sys.stdout.write("\033[B")
 
 # Cursor
 
@@ -65,3 +65,27 @@ sys.stdout.write("\033[100A")
 # Homework
 # Make the box draw in the right place
 # Allow coordinates so that they can draw inside of each other.
+
+if __name__ == "__main__":
+
+    terminal_width = os.get_terminal_size()[0]
+    terminal_height = os.get_terminal_size()[1]
+
+    # use function
+
+    clear()
+
+    # Saves current location
+    sys.stdout.write("\033[s")
+
+    coords = [0,0]
+    draw_box(width=terminal_width-1,height=terminal_height-5, makeSquare=False, coords=coords)
+
+    # Return to saved spot
+    sys.stdout.write("\033[u")
+
+    coords = [0,terminal_height-1]
+    draw_box(terminal_width,5, coords=coords)
+
+    sys.stdout.write("\033[100A")
+    input()
